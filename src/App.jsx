50c58fee0bbcdf5ce9a66ce7d6ebb210e2016383
Marcos Willian <87791042+marcosmwx@users.noreply.github.com>
2023-01-26 00:22:06 -0300

    Site update

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+import "./App.css";
+import video from "./assets/video/video.mp4";
+
+function App() {
+  function copiar(event) {
+    const button = event.target;
+    const summary = button.parentNode;
+    const details = summary.parentNode;
+    const content = details.querySelector("p");
+    navigator.clipboard.writeText(content.textContent);
+  }
+
+  return (
+    <div className="App">
+      <div className="video">
+        <video src={video} autoPlay loop muted />
+
+        <div className="detalhes">
+          <details>
+            <summary>Quais os 10 Times com mais Vitórias na Temporada?</summary>
+            <div>
+              <p className="copie">
+                SELECT DISTINCT * from rankin where SEASON_ID= '22018'ORDER BY W
+                DESC;
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+
+          <details>
+            <summary>Quais os Times que tem mais Vitórias em casa?</summary>
+            <div>
+              <p>
+                SELECT DISTINCT HOME_RECORD, TEAM FROM rankin where SEASON_ID=
+                '22018'ORDER BY HOME_RECORD DESC;
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+
+          <details>
+            <summary>
+              Quais são os Times com Mais Vitórias Fora de Casa?
+            </summary>
+            <div>
+              <p>
+                SELECT DISTINCT ROAD_RECORD, TEAM FROM rankin WHERE SEASON_ID=
+                '22018'ORDER BY ROAD_RECORD DESC;
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+
+          <details>
+            <summary>Qual time tem mais assistência na SEASON?</summary>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Quibusda
+                m dicta sapiente totam libero non repellat natus, ducimus,
+                soluta veritatis inventore reprehenderit maiores maxime corrupti
+                Voluptatibus nisi explicabo ab tempore obcaecati?
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+
+          <details>
+            <summary> Qual jogador tem mais assistência na SEASON?</summary>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Quibusda
+                m dicta sapiente totam libero non repellat natus, ducimus,
+                soluta veritatis inventore reprehenderit maiores maxime corrupti
+                Voluptatibus nisi explicabo ab tempore obcaecati?
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+
+          <details>
+            <summary> Qual jogador fez mais rebote na SEASON?</summary>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Quibusda
+                m dicta sapiente totam libero non repellat natus, ducimus,
+                soluta veritatis inventore reprehenderit maiores maxime corrupti
+                Voluptatibus nisi explicabo ab tempore obcaecati?
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+
+          <details>
+            <summary> Qual time fez mais rebote na SEASON?</summary>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Quibusda
+                m dicta sapiente totam libero non repellat natus, ducimus,
+                soluta veritatis inventore reprehenderit maiores maxime corrupti
+                Voluptatibus nisi explicabo ab tempore obcaecati?
+              </p>
+              <button className="button-cp" onClick={copiar}>
+                Copiar
+              </button>
+            </div>
+          </details>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
